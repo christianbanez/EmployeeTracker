c300984b5b3e718c572bfed0aeb93f237cebd9bc
JazzyWhizzy <jazminedizon26@gmail.com>
2024-03-24 00:28:34 +0800

    Updated search onclick method

--- EmployeeTracker/Form1.Designer.cs
+++ EmployeeTracker/Form1.Designer.cs
@@ -70,9 +70,10 @@ namespace EmployeeTracker
             this.panel1.Controls.Add(this.textSearch);
             this.panel1.Controls.Add(this.btnAdd);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(7, 12);
+            this.panel1.Location = new System.Drawing.Point(5, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1233, 525);
+            this.panel1.Size = new System.Drawing.Size(925, 427);
             this.panel1.TabIndex = 0;
             // 
             // displayData
@@ -83,22 +84,22 @@ namespace EmployeeTracker
             this.displayData.AllowUserToResizeRows = false;
             this.displayData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.displayData.ColumnHeadersHeight = 34;
-            this.displayData.Location = new System.Drawing.Point(28, 72);
-            this.displayData.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.displayData.Location = new System.Drawing.Point(21, 58);
+            this.displayData.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.displayData.Name = "displayData";
             this.displayData.ReadOnly = true;
             this.displayData.RowHeadersWidth = 62;
             this.displayData.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
-            this.displayData.Size = new System.Drawing.Size(720, 406);
+            this.displayData.Size = new System.Drawing.Size(540, 330);
             this.displayData.TabIndex = 11;
             this.displayData.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.displayData_CellClick);
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(1092, 478);
-            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnExit.Location = new System.Drawing.Point(819, 388);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(117, 25);
+            this.btnExit.Size = new System.Drawing.Size(88, 20);
             this.btnExit.TabIndex = 8;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -106,20 +107,20 @@ namespace EmployeeTracker
             // 
             // btnPrint
             // 
-            this.btnPrint.Location = new System.Drawing.Point(1057, 423);
-            this.btnPrint.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnPrint.Location = new System.Drawing.Point(793, 344);
+            this.btnPrint.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnPrint.Name = "btnPrint";
-            this.btnPrint.Size = new System.Drawing.Size(117, 25);
+            this.btnPrint.Size = new System.Drawing.Size(88, 20);
             this.btnPrint.TabIndex = 7;
             this.btnPrint.Text = "Print";
             this.btnPrint.UseVisualStyleBackColor = true;
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(934, 423);
-            this.btnDelete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDelete.Location = new System.Drawing.Point(700, 344);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(117, 25);
+            this.btnDelete.Size = new System.Drawing.Size(88, 20);
             this.btnDelete.TabIndex = 5;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
@@ -127,20 +128,21 @@ namespace EmployeeTracker
             // 
             // textSearch
             // 
-            this.textSearch.Location = new System.Drawing.Point(38, 45);
-            this.textSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textSearch.Location = new System.Drawing.Point(28, 37);
+            this.textSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textSearch.Name = "textSearch";
-            this.textSearch.Size = new System.Drawing.Size(565, 22);
+            this.textSearch.Size = new System.Drawing.Size(425, 20);
             this.textSearch.TabIndex = 3;
             this.textSearch.Text = "Search...";
+            this.textSearch.Click += new System.EventHandler(this.textSearch_Click);
             this.textSearch.TextChanged += new System.EventHandler(this.textsearch_textchanged);
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(811, 423);
-            this.btnAdd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAdd.Location = new System.Drawing.Point(608, 344);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(117, 25);
+            this.btnAdd.Size = new System.Drawing.Size(88, 20);
             this.btnAdd.TabIndex = 2;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -165,64 +167,66 @@ namespace EmployeeTracker
             this.panel2.Controls.Add(this.txtAge);
             this.panel2.Controls.Add(this.txtContact);
             this.panel2.Controls.Add(this.txtfName);
-            this.panel2.Location = new System.Drawing.Point(767, 102);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Location = new System.Drawing.Point(575, 83);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(442, 306);
+            this.panel2.Size = new System.Drawing.Size(332, 249);
             this.panel2.TabIndex = 1;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_paint);
             // 
             // txtrole
             // 
-            this.txtrole.Location = new System.Drawing.Point(156, 209);
-            this.txtrole.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtrole.Location = new System.Drawing.Point(117, 170);
+            this.txtrole.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtrole.Name = "txtrole";
-            this.txtrole.Size = new System.Drawing.Size(224, 22);
+            this.txtrole.Size = new System.Drawing.Size(169, 20);
             this.txtrole.TabIndex = 26;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(52, 212);
+            this.label8.Location = new System.Drawing.Point(39, 172);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(36, 16);
+            this.label8.Size = new System.Drawing.Size(29, 13);
             this.label8.TabIndex = 25;
             this.label8.Text = "Role";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(52, 51);
+            this.label6.Location = new System.Drawing.Point(39, 41);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(72, 16);
+            this.label6.Size = new System.Drawing.Size(57, 13);
             this.label6.TabIndex = 23;
             this.label6.Text = "First Name";
             // 
             // txtlName
             // 
-            this.txtlName.Location = new System.Drawing.Point(156, 79);
-            this.txtlName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtlName.Location = new System.Drawing.Point(117, 64);
+            this.txtlName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtlName.Name = "txtlName";
-            this.txtlName.Size = new System.Drawing.Size(224, 22);
+            this.txtlName.Size = new System.Drawing.Size(169, 20);
             this.txtlName.TabIndex = 22;
             // 
             // chkActive
             // 
             this.chkActive.AutoSize = true;
-            this.chkActive.Location = new System.Drawing.Point(156, 255);
-            this.chkActive.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.chkActive.Location = new System.Drawing.Point(117, 207);
+            this.chkActive.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chkActive.Name = "chkActive";
-            this.chkActive.Size = new System.Drawing.Size(66, 20);
+            this.chkActive.Size = new System.Drawing.Size(56, 17);
             this.chkActive.TabIndex = 21;
             this.chkActive.Text = "Active";
             this.chkActive.UseVisualStyleBackColor = true;
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(290, 262);
-            this.btnUpdate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnUpdate.Location = new System.Drawing.Point(218, 213);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(117, 25);
+            this.btnUpdate.Size = new System.Drawing.Size(88, 20);
             this.btnUpdate.TabIndex = 4;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
@@ -231,99 +235,105 @@ namespace EmployeeTracker
             // txtEmployeeID
             // 
             this.txtEmployeeID.Enabled = false;
-            this.txtEmployeeID.Location = new System.Drawing.Point(156, 18);
-            this.txtEmployeeID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtEmployeeID.Location = new System.Drawing.Point(117, 15);
+            this.txtEmployeeID.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtEmployeeID.Name = "txtEmployeeID";
-            this.txtEmployeeID.Size = new System.Drawing.Size(224, 22);
+            this.txtEmployeeID.Size = new System.Drawing.Size(169, 20);
             this.txtEmployeeID.TabIndex = 20;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(52, 21);
+            this.label5.Location = new System.Drawing.Point(39, 17);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(85, 16);
+            this.label5.Size = new System.Drawing.Size(67, 13);
             this.label5.TabIndex = 19;
             this.label5.Text = "Employee ID";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(52, 182);
+            this.label4.Location = new System.Drawing.Point(39, 148);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.Size = new System.Drawing.Size(32, 13);
             this.label4.TabIndex = 18;
             this.label4.Text = "Email";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(52, 145);
+            this.label3.Location = new System.Drawing.Point(39, 118);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(32, 16);
+            this.label3.Size = new System.Drawing.Size(26, 13);
             this.label3.TabIndex = 17;
             this.label3.Text = "Age";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 113);
+            this.label2.Location = new System.Drawing.Point(39, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.Size = new System.Drawing.Size(44, 13);
             this.label2.TabIndex = 16;
             this.label2.Text = "Contact";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 82);
+            this.label1.Location = new System.Drawing.Point(39, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 15;
             this.label1.Text = "Last Name";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(156, 176);
-            this.txtEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtEmail.Location = new System.Drawing.Point(117, 143);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(224, 22);
+            this.txtEmail.Size = new System.Drawing.Size(169, 20);
             this.txtEmail.TabIndex = 13;
             // 
             // txtAge
             // 
-            this.txtAge.Location = new System.Drawing.Point(156, 142);
-            this.txtAge.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtAge.Location = new System.Drawing.Point(117, 115);
+            this.txtAge.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtAge.Name = "txtAge";
-            this.txtAge.Size = new System.Drawing.Size(224, 22);
+            this.txtAge.Size = new System.Drawing.Size(169, 20);
             this.txtAge.TabIndex = 12;
             // 
             // txtContact
             // 
-            this.txtContact.Location = new System.Drawing.Point(156, 110);
-            this.txtContact.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtContact.Location = new System.Drawing.Point(117, 89);
+            this.txtContact.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtContact.MaxLength = 11;
             this.txtContact.Name = "txtContact";
-            this.txtContact.Size = new System.Drawing.Size(224, 22);
+            this.txtContact.Size = new System.Drawing.Size(169, 20);
             this.txtContact.TabIndex = 11;
             this.txtContact.TextChanged += new System.EventHandler(this.txtContact_TextChanged);
             this.txtContact.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtcontact_keypress);
             // 
             // txtfName
             // 
-            this.txtfName.Location = new System.Drawing.Point(156, 51);
-            this.txtfName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtfName.Location = new System.Drawing.Point(117, 41);
+            this.txtfName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtfName.Name = "txtfName";
-            this.txtfName.Size = new System.Drawing.Size(224, 22);
+            this.txtfName.Size = new System.Drawing.Size(169, 20);
             this.txtfName.TabIndex = 10;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1247, 549);
+            this.ClientSize = new System.Drawing.Size(935, 446);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- EmployeeTracker/Form1.cs
+++ EmployeeTracker/Form1.cs
@@ -16,7 +16,7 @@ namespace EmployeeTracker
 {
     public partial class Form1 : Form
     {
-        OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\tdizon\Downloads\dbtk.accdb");
+        OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\4. OJT\Jazmine\EmployeeTracker\dbtk.accdb");
         DataTable dt;
         int state;
         public Form1()
@@ -297,5 +297,9 @@ namespace EmployeeTracker
             }
         }
 
+        private void textSearch_Click(object sender, EventArgs e)
+        {
+            textSearch.Text = "";
+        }
     }
 }
