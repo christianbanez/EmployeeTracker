3fd84150f3a5f30dacbbbeac2a384fb552fa9a2f
Christian John Banez <christianjohn.banez@infor.com>
2024-03-24 15:14:50 +0800

    error trapping for add done

--- EmployeeTracker/Form2.Designer.cs
+++ EmployeeTracker/Form2.Designer.cs
@@ -28,12 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panel2 = new System.Windows.Forms.Panel();
             this.btnAdd = new System.Windows.Forms.Button();
             this.txtrole = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.txtfName = new System.Windows.Forms.TextBox();
+            this.txtlName = new System.Windows.Forms.TextBox();
             this.chkActive = new System.Windows.Forms.CheckBox();
             this.txtEmployeeID = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
@@ -44,8 +45,22 @@
             this.txtEmail = new System.Windows.Forms.TextBox();
             this.txtAge = new System.Windows.Forms.TextBox();
             this.txtContact = new System.Windows.Forms.TextBox();
-            this.txtlName = new System.Windows.Forms.TextBox();
+            this.txtfName = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider3 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider4 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider5 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider6 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider7 = new System.Windows.Forms.ErrorProvider(this.components);
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider7)).BeginInit();
             this.SuspendLayout();
             // 
             // panel2
@@ -55,7 +70,7 @@
             this.panel2.Controls.Add(this.txtrole);
             this.panel2.Controls.Add(this.label8);
             this.panel2.Controls.Add(this.label6);
-            this.panel2.Controls.Add(this.txtfName);
+            this.panel2.Controls.Add(this.txtlName);
             this.panel2.Controls.Add(this.chkActive);
             this.panel2.Controls.Add(this.txtEmployeeID);
             this.panel2.Controls.Add(this.label5);
@@ -66,19 +81,19 @@
             this.panel2.Controls.Add(this.txtEmail);
             this.panel2.Controls.Add(this.txtAge);
             this.panel2.Controls.Add(this.txtContact);
-            this.panel2.Controls.Add(this.txtlName);
-            this.panel2.Location = new System.Drawing.Point(11, 9);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Controls.Add(this.txtfName);
+            this.panel2.Location = new System.Drawing.Point(8, 7);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(442, 306);
+            this.panel2.Size = new System.Drawing.Size(332, 249);
             this.panel2.TabIndex = 2;
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(263, 252);
-            this.btnAdd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAdd.Location = new System.Drawing.Point(197, 205);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(117, 25);
+            this.btnAdd.Size = new System.Drawing.Size(88, 20);
             this.btnAdd.TabIndex = 27;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -86,146 +101,200 @@
             // 
             // txtrole
             // 
-            this.txtrole.Location = new System.Drawing.Point(156, 209);
-            this.txtrole.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtrole.Location = new System.Drawing.Point(117, 170);
+            this.txtrole.Margin = new System.Windows.Forms.Padding(2);
             this.txtrole.Name = "txtrole";
-            this.txtrole.Size = new System.Drawing.Size(224, 22);
+            this.txtrole.Size = new System.Drawing.Size(169, 20);
             this.txtrole.TabIndex = 26;
+            this.txtrole.Validating += new System.ComponentModel.CancelEventHandler(this.txtrole_Validating);
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(52, 212);
+            this.label8.Location = new System.Drawing.Point(39, 172);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(36, 16);
+            this.label8.Size = new System.Drawing.Size(29, 13);
             this.label8.TabIndex = 25;
             this.label8.Text = "Role";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(52, 51);
+            this.label6.Location = new System.Drawing.Point(39, 41);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(72, 16);
+            this.label6.Size = new System.Drawing.Size(57, 13);
             this.label6.TabIndex = 23;
             this.label6.Text = "First Name";
             // 
-            // txtfName
+            // txtlName
             // 
-            this.txtfName.Location = new System.Drawing.Point(156, 79);
-            this.txtfName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtfName.Name = "txtfName";
-            this.txtfName.Size = new System.Drawing.Size(224, 22);
-            this.txtfName.TabIndex = 22;
+            this.txtlName.Location = new System.Drawing.Point(117, 64);
+            this.txtlName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtlName.Name = "txtlName";
+            this.txtlName.Size = new System.Drawing.Size(169, 20);
+            this.txtlName.TabIndex = 22;
+            this.txtlName.Validating += new System.ComponentModel.CancelEventHandler(this.txtlName_Validating_1);
             // 
             // chkActive
             // 
             this.chkActive.AutoSize = true;
-            this.chkActive.Location = new System.Drawing.Point(156, 255);
-            this.chkActive.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.chkActive.Location = new System.Drawing.Point(117, 207);
+            this.chkActive.Margin = new System.Windows.Forms.Padding(2);
             this.chkActive.Name = "chkActive";
-            this.chkActive.Size = new System.Drawing.Size(66, 20);
+            this.chkActive.Size = new System.Drawing.Size(56, 17);
             this.chkActive.TabIndex = 21;
             this.chkActive.Text = "Active";
             this.chkActive.UseVisualStyleBackColor = true;
             // 
             // txtEmployeeID
             // 
-            this.txtEmployeeID.Location = new System.Drawing.Point(156, 18);
-            this.txtEmployeeID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtEmployeeID.Location = new System.Drawing.Point(117, 15);
+            this.txtEmployeeID.Margin = new System.Windows.Forms.Padding(2);
             this.txtEmployeeID.Name = "txtEmployeeID";
-            this.txtEmployeeID.Size = new System.Drawing.Size(224, 22);
+            this.txtEmployeeID.Size = new System.Drawing.Size(169, 20);
             this.txtEmployeeID.TabIndex = 20;
+            this.txtEmployeeID.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtEmployeeID_KeyPress);
+            this.txtEmployeeID.Leave += new System.EventHandler(this.txtEmployeeID_Leave);
+            this.txtEmployeeID.Validating += new System.ComponentModel.CancelEventHandler(this.txtEmployeeID_Validating);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(52, 21);
+            this.label5.Location = new System.Drawing.Point(39, 17);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(85, 16);
+            this.label5.Size = new System.Drawing.Size(67, 13);
             this.label5.TabIndex = 19;
             this.label5.Text = "Employee ID";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(52, 182);
+            this.label4.Location = new System.Drawing.Point(39, 148);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.Size = new System.Drawing.Size(32, 13);
             this.label4.TabIndex = 18;
             this.label4.Text = "Email";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(52, 145);
+            this.label3.Location = new System.Drawing.Point(39, 118);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(32, 16);
+            this.label3.Size = new System.Drawing.Size(26, 13);
             this.label3.TabIndex = 17;
             this.label3.Text = "Age";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 113);
+            this.label2.Location = new System.Drawing.Point(39, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.Size = new System.Drawing.Size(44, 13);
             this.label2.TabIndex = 16;
             this.label2.Text = "Contact";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 82);
+            this.label1.Location = new System.Drawing.Point(39, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 15;
             this.label1.Text = "Last Name";
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(156, 176);
-            this.txtEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtEmail.Location = new System.Drawing.Point(117, 143);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(224, 22);
+            this.txtEmail.Size = new System.Drawing.Size(169, 20);
             this.txtEmail.TabIndex = 13;
+            this.txtEmail.Validating += new System.ComponentModel.CancelEventHandler(this.txtEmail_Validating);
             // 
             // txtAge
             // 
-            this.txtAge.Location = new System.Drawing.Point(156, 142);
-            this.txtAge.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtAge.Location = new System.Drawing.Point(117, 115);
+            this.txtAge.Margin = new System.Windows.Forms.Padding(2);
             this.txtAge.Name = "txtAge";
-            this.txtAge.Size = new System.Drawing.Size(224, 22);
+            this.txtAge.Size = new System.Drawing.Size(169, 20);
             this.txtAge.TabIndex = 12;
+            this.txtAge.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAge_KeyPress);
+            this.txtAge.Validating += new System.ComponentModel.CancelEventHandler(this.txtAge_Validating);
             // 
             // txtContact
             // 
-            this.txtContact.Location = new System.Drawing.Point(156, 110);
-            this.txtContact.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtContact.Location = new System.Drawing.Point(117, 89);
+            this.txtContact.Margin = new System.Windows.Forms.Padding(2);
             this.txtContact.MaxLength = 11;
             this.txtContact.Name = "txtContact";
-            this.txtContact.Size = new System.Drawing.Size(224, 22);
+            this.txtContact.Size = new System.Drawing.Size(169, 20);
             this.txtContact.TabIndex = 11;
+            this.txtContact.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtContact_KeyPress);
+            this.txtContact.Validating += new System.ComponentModel.CancelEventHandler(this.txtContact_Validating);
             // 
-            // txtlName
+            // txtfName
             // 
-            this.txtlName.Location = new System.Drawing.Point(156, 48);
-            this.txtlName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtlName.Name = "txtlName";
-            this.txtlName.Size = new System.Drawing.Size(224, 22);
-            this.txtlName.TabIndex = 10;
+            this.txtfName.Location = new System.Drawing.Point(117, 39);
+            this.txtfName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtfName.Name = "txtfName";
+            this.txtfName.Size = new System.Drawing.Size(169, 20);
+            this.txtfName.TabIndex = 10;
+            this.txtfName.Leave += new System.EventHandler(this.txtlName_Leave);
+            this.txtfName.Validating += new System.ComponentModel.CancelEventHandler(this.txtlName_Validating);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // errorProvider3
+            // 
+            this.errorProvider3.ContainerControl = this;
+            // 
+            // errorProvider4
+            // 
+            this.errorProvider4.ContainerControl = this;
+            // 
+            // errorProvider5
+            // 
+            this.errorProvider5.ContainerControl = this;
+            // 
+            // errorProvider6
+            // 
+            this.errorProvider6.ContainerControl = this;
+            // 
+            // errorProvider7
+            // 
+            this.errorProvider7.ContainerControl = this;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(460, 322);
+            this.ClientSize = new System.Drawing.Size(345, 262);
             this.Controls.Add(this.panel2);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form2";
             this.Text = "Form2";
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider7)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -236,7 +305,7 @@
         private System.Windows.Forms.TextBox txtrole;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox txtfName;
+        private System.Windows.Forms.TextBox txtlName;
         private System.Windows.Forms.CheckBox chkActive;
         private System.Windows.Forms.TextBox txtEmployeeID;
         private System.Windows.Forms.Label label5;
@@ -247,7 +316,14 @@
         private System.Windows.Forms.TextBox txtEmail;
         private System.Windows.Forms.TextBox txtAge;
         private System.Windows.Forms.TextBox txtContact;
-        private System.Windows.Forms.TextBox txtlName;
+        private System.Windows.Forms.TextBox txtfName;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.ErrorProvider errorProvider3;
+        private System.Windows.Forms.ErrorProvider errorProvider4;
+        private System.Windows.Forms.ErrorProvider errorProvider5;
+        private System.Windows.Forms.ErrorProvider errorProvider6;
+        private System.Windows.Forms.ErrorProvider errorProvider7;
     }
 }
