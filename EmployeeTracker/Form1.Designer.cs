5c1d47de7b3624450ffe47d0a6b6bf7bddb48615
JazzyWhizzy <jazminedizon26@gmail.com>
2024-03-25 16:01:34 +0800

    Dashboard UI fix

--- EmployeeTracker/Form1.Designer.cs
+++ EmployeeTracker/Form1.Designer.cs
@@ -29,27 +29,27 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
+            this.btnAddTsk = new System.Windows.Forms.Button();
             this.panel6 = new System.Windows.Forms.Panel();
-            this.btnOverV = new System.Windows.Forms.Button();
-            this.btnToday = new System.Windows.Forms.Button();
-            this.btnCalendar = new System.Windows.Forms.Button();
             this.btnTeam = new System.Windows.Forms.Button();
+            this.btnCalendar = new System.Windows.Forms.Button();
+            this.btnToday = new System.Windows.Forms.Button();
+            this.btnOverV = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.panel7 = new System.Windows.Forms.Panel();
             this.btnAddEm = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.btnExport = new System.Windows.Forms.Button();
-            this.btnAddTsk = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
-            this.panel4.SuspendLayout();
             this.panel5.SuspendLayout();
             this.panel6.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.panel7.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -63,48 +63,24 @@
             this.panel1.Size = new System.Drawing.Size(1237, 648);
             this.panel1.TabIndex = 0;
             // 
-            // monthCalendar1
-            // 
-            this.monthCalendar1.Location = new System.Drawing.Point(9, 12);
-            this.monthCalendar1.Name = "monthCalendar1";
-            this.monthCalendar1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.btnExport);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1237, 46);
-            this.panel2.TabIndex = 1;
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.monthCalendar1);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel3.Location = new System.Drawing.Point(0, 46);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(280, 602);
-            this.panel3.TabIndex = 2;
-            // 
-            // panel4
-            // 
-            this.panel4.Controls.Add(this.panel7);
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel4.Location = new System.Drawing.Point(972, 46);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(265, 602);
-            this.panel4.TabIndex = 3;
-            // 
             // panel5
             // 
             this.panel5.Controls.Add(this.btnAddTsk);
             this.panel5.Controls.Add(this.panel6);
             this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel5.Location = new System.Drawing.Point(280, 46);
+            this.panel5.Location = new System.Drawing.Point(280, 40);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(692, 602);
+            this.panel5.Size = new System.Drawing.Size(692, 608);
             this.panel5.TabIndex = 4;
+            // 
+            // btnAddTsk
+            // 
+            this.btnAddTsk.Location = new System.Drawing.Point(15, 59);
+            this.btnAddTsk.Name = "btnAddTsk";
+            this.btnAddTsk.Size = new System.Drawing.Size(121, 30);
+            this.btnAddTsk.TabIndex = 5;
+            this.btnAddTsk.Text = "Add Task";
+            this.btnAddTsk.UseVisualStyleBackColor = true;
             // 
             // panel6
             // 
@@ -118,23 +94,14 @@
             this.panel6.Size = new System.Drawing.Size(692, 45);
             this.panel6.TabIndex = 0;
             // 
-            // btnOverV
+            // btnTeam
             // 
-            this.btnOverV.Location = new System.Drawing.Point(6, 12);
-            this.btnOverV.Name = "btnOverV";
-            this.btnOverV.Size = new System.Drawing.Size(75, 30);
-            this.btnOverV.TabIndex = 0;
-            this.btnOverV.Text = "Overview";
-            this.btnOverV.UseVisualStyleBackColor = true;
-            // 
-            // btnToday
-            // 
-            this.btnToday.Location = new System.Drawing.Point(87, 12);
-            this.btnToday.Name = "btnToday";
-            this.btnToday.Size = new System.Drawing.Size(75, 30);
-            this.btnToday.TabIndex = 1;
-            this.btnToday.Text = "Today";
-            this.btnToday.UseVisualStyleBackColor = true;
+            this.btnTeam.Location = new System.Drawing.Point(249, 12);
+            this.btnTeam.Name = "btnTeam";
+            this.btnTeam.Size = new System.Drawing.Size(75, 30);
+            this.btnTeam.TabIndex = 3;
+            this.btnTeam.Text = "Team";
+            this.btnTeam.UseVisualStyleBackColor = true;
             // 
             // btnCalendar
             // 
@@ -145,14 +112,32 @@
             this.btnCalendar.Text = "Calendar";
             this.btnCalendar.UseVisualStyleBackColor = true;
             // 
-            // btnTeam
+            // btnToday
             // 
-            this.btnTeam.Location = new System.Drawing.Point(249, 12);
-            this.btnTeam.Name = "btnTeam";
-            this.btnTeam.Size = new System.Drawing.Size(75, 30);
-            this.btnTeam.TabIndex = 3;
-            this.btnTeam.Text = "Team";
-            this.btnTeam.UseVisualStyleBackColor = true;
+            this.btnToday.Location = new System.Drawing.Point(87, 12);
+            this.btnToday.Name = "btnToday";
+            this.btnToday.Size = new System.Drawing.Size(75, 30);
+            this.btnToday.TabIndex = 1;
+            this.btnToday.Text = "Today";
+            this.btnToday.UseVisualStyleBackColor = true;
+            // 
+            // btnOverV
+            // 
+            this.btnOverV.Location = new System.Drawing.Point(6, 12);
+            this.btnOverV.Name = "btnOverV";
+            this.btnOverV.Size = new System.Drawing.Size(75, 30);
+            this.btnOverV.TabIndex = 0;
+            this.btnOverV.Text = "Overview";
+            this.btnOverV.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.panel7);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(972, 40);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(265, 608);
+            this.panel4.TabIndex = 3;
             // 
             // panel7
             // 
@@ -172,23 +157,38 @@
             this.btnAddEm.Text = "Add Employee";
             this.btnAddEm.UseVisualStyleBackColor = true;
             // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.monthCalendar1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 40);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(280, 608);
+            this.panel3.TabIndex = 2;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(9, 12);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnExport);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1237, 40);
+            this.panel2.TabIndex = 1;
+            // 
             // btnExport
             // 
-            this.btnExport.Location = new System.Drawing.Point(1125, 12);
+            this.btnExport.Location = new System.Drawing.Point(1130, 5);
             this.btnExport.Name = "btnExport";
             this.btnExport.Size = new System.Drawing.Size(75, 30);
             this.btnExport.TabIndex = 4;
             this.btnExport.Text = "Export";
             this.btnExport.UseVisualStyleBackColor = true;
-            // 
-            // btnAddTsk
-            // 
-            this.btnAddTsk.Location = new System.Drawing.Point(15, 59);
-            this.btnAddTsk.Name = "btnAddTsk";
-            this.btnAddTsk.Size = new System.Drawing.Size(121, 30);
-            this.btnAddTsk.TabIndex = 5;
-            this.btnAddTsk.Text = "Add Task";
-            this.btnAddTsk.UseVisualStyleBackColor = true;
             // 
             // frmDashboard
             // 
@@ -200,12 +200,12 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Employee Tracker";
             this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
-            this.panel4.ResumeLayout(false);
             this.panel5.ResumeLayout(false);
             this.panel6.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
             this.panel7.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
