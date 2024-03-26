3bfa4ed1d39fd73fc63f42fa1bfb530bbdd143e4
jsantiago3 <jsantiago3@PHHWNJSANTIAG01>
2024-03-26 11:55:46 +0800

    Done with task,title,desc and assign

--- EmployeeTracker/Form1.Designer.cs
+++ EmployeeTracker/Form1.Designer.cs
@@ -40,12 +40,12 @@
             this.ltskDesc = new System.Windows.Forms.Label();
             this.lbtskTitle = new System.Windows.Forms.Label();
             this.cmbxProj = new System.Windows.Forms.ComboBox();
+            this.dateTimePickerStartDate = new System.Windows.Forms.DateTimePicker();
+            this.cmbxAssign = new System.Windows.Forms.ComboBox();
             this.employeeBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.dbtkDataSet = new EmployeeTracker.dbtkDataSet();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.cmbxAssign = new System.Windows.Forms.ComboBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtTaskDesc = new System.Windows.Forms.TextBox();
+            this.txtTaskName = new System.Windows.Forms.TextBox();
             this.employeeTableAdapter = new EmployeeTracker.dbtkDataSetTableAdapters.EmployeeTableAdapter();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).BeginInit();
@@ -64,22 +64,23 @@
             this.panel1.Controls.Add(this.ltskDesc);
             this.panel1.Controls.Add(this.lbtskTitle);
             this.panel1.Controls.Add(this.cmbxProj);
-            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.dateTimePickerStartDate);
             this.panel1.Controls.Add(this.cmbxAssign);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Controls.Add(this.txtTaskDesc);
+            this.panel1.Controls.Add(this.txtTaskName);
+            this.panel1.Location = new System.Drawing.Point(15, 16);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(738, 531);
+            this.panel1.Size = new System.Drawing.Size(830, 664);
             this.panel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(566, 33);
+            this.label1.Location = new System.Drawing.Point(637, 41);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 25);
+            this.label1.Size = new System.Drawing.Size(78, 29);
             this.label1.TabIndex = 13;
             this.label1.Text = "Color:";
             // 
@@ -88,36 +89,40 @@
             this.cmbxColor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbxColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbxColor.FormattingEnabled = true;
-            this.cmbxColor.Location = new System.Drawing.Point(571, 65);
+            this.cmbxColor.Location = new System.Drawing.Point(642, 81);
+            this.cmbxColor.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cmbxColor.Name = "cmbxColor";
-            this.cmbxColor.Size = new System.Drawing.Size(139, 33);
+            this.cmbxColor.Size = new System.Drawing.Size(156, 37);
             this.cmbxColor.TabIndex = 12;
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(592, 481);
+            this.btnCancel.Location = new System.Drawing.Point(666, 601);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(128, 25);
+            this.btnCancel.Size = new System.Drawing.Size(144, 31);
             this.btnCancel.TabIndex = 11;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(431, 481);
+            this.btnAdd.Location = new System.Drawing.Point(485, 601);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(128, 25);
+            this.btnAdd.Size = new System.Drawing.Size(144, 31);
             this.btnAdd.TabIndex = 10;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // lStartD
             // 
             this.lStartD.AutoSize = true;
             this.lStartD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lStartD.Location = new System.Drawing.Point(12, 98);
+            this.lStartD.Location = new System.Drawing.Point(14, 122);
             this.lStartD.Name = "lStartD";
-            this.lStartD.Size = new System.Drawing.Size(105, 25);
+            this.lStartD.Size = new System.Drawing.Size(124, 29);
             this.lStartD.TabIndex = 8;
             this.lStartD.Text = "Start Date:";
             // 
@@ -125,9 +130,9 @@
             // 
             this.lProj.AutoSize = true;
             this.lProj.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lProj.Location = new System.Drawing.Point(387, 380);
+            this.lProj.Location = new System.Drawing.Point(435, 475);
             this.lProj.Name = "lProj";
-            this.lProj.Size = new System.Drawing.Size(78, 25);
+            this.lProj.Size = new System.Drawing.Size(95, 29);
             this.lProj.TabIndex = 9;
             this.lProj.Text = "Project:";
             // 
@@ -135,9 +140,9 @@
             // 
             this.lAssign.AutoSize = true;
             this.lAssign.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lAssign.Location = new System.Drawing.Point(12, 380);
+            this.lAssign.Location = new System.Drawing.Point(14, 475);
             this.lAssign.Name = "lAssign";
-            this.lAssign.Size = new System.Drawing.Size(129, 25);
+            this.lAssign.Size = new System.Drawing.Size(155, 29);
             this.lAssign.TabIndex = 7;
             this.lAssign.Text = "Assigned To:";
             // 
@@ -145,9 +150,9 @@
             // 
             this.ltskDesc.AutoSize = true;
             this.ltskDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ltskDesc.Location = new System.Drawing.Point(12, 163);
+            this.ltskDesc.Location = new System.Drawing.Point(14, 204);
             this.ltskDesc.Name = "ltskDesc";
-            this.ltskDesc.Size = new System.Drawing.Size(164, 25);
+            this.ltskDesc.Size = new System.Drawing.Size(200, 29);
             this.ltskDesc.TabIndex = 8;
             this.ltskDesc.Text = "Task Description:";
             // 
@@ -155,9 +160,9 @@
             // 
             this.lbtskTitle.AutoSize = true;
             this.lbtskTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbtskTitle.Location = new System.Drawing.Point(12, 33);
+            this.lbtskTitle.Location = new System.Drawing.Point(14, 41);
             this.lbtskTitle.Name = "lbtskTitle";
-            this.lbtskTitle.Size = new System.Drawing.Size(104, 25);
+            this.lbtskTitle.Size = new System.Drawing.Size(126, 29);
             this.lbtskTitle.TabIndex = 6;
             this.lbtskTitle.Text = "Task Title:";
             // 
@@ -166,10 +171,36 @@
             this.cmbxProj.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbxProj.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbxProj.FormattingEnabled = true;
-            this.cmbxProj.Location = new System.Drawing.Point(392, 412);
+            this.cmbxProj.Location = new System.Drawing.Point(441, 515);
+            this.cmbxProj.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cmbxProj.Name = "cmbxProj";
-            this.cmbxProj.Size = new System.Drawing.Size(293, 33);
+            this.cmbxProj.Size = new System.Drawing.Size(329, 37);
             this.cmbxProj.TabIndex = 5;
+            // 
+            // dateTimePickerStartDate
+            // 
+            this.dateTimePickerStartDate.CustomFormat = "ddd, MM/dd/yyyy, hh:mm tt";
+            this.dateTimePickerStartDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerStartDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerStartDate.Location = new System.Drawing.Point(19, 162);
+            this.dateTimePickerStartDate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dateTimePickerStartDate.Name = "dateTimePickerStartDate";
+            this.dateTimePickerStartDate.Size = new System.Drawing.Size(355, 35);
+            this.dateTimePickerStartDate.TabIndex = 4;
+            // 
+            // cmbxAssign
+            // 
+            this.cmbxAssign.DataSource = this.employeeBindingSource;
+            this.cmbxAssign.DisplayMember = "fName";
+            this.cmbxAssign.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbxAssign.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbxAssign.FormattingEnabled = true;
+            this.cmbxAssign.Location = new System.Drawing.Point(19, 515);
+            this.cmbxAssign.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cmbxAssign.Name = "cmbxAssign";
+            this.cmbxAssign.Size = new System.Drawing.Size(355, 37);
+            this.cmbxAssign.TabIndex = 3;
+            this.cmbxAssign.SelectedIndexChanged += new System.EventHandler(this.cmbxAssign_SelectedIndexChanged);
             // 
             // employeeBindingSource
             // 
@@ -181,44 +212,24 @@
             this.dbtkDataSet.DataSetName = "dbtkDataSet";
             this.dbtkDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
-            // dateTimePicker1
+            // txtTaskDesc
             // 
-            this.dateTimePicker1.CustomFormat = "ddd, MM/dd/yyyy";
-            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(17, 130);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(212, 30);
-            this.dateTimePicker1.TabIndex = 4;
+            this.txtTaskDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTaskDesc.Location = new System.Drawing.Point(19, 244);
+            this.txtTaskDesc.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtTaskDesc.Multiline = true;
+            this.txtTaskDesc.Name = "txtTaskDesc";
+            this.txtTaskDesc.Size = new System.Drawing.Size(790, 226);
+            this.txtTaskDesc.TabIndex = 1;
             // 
-            // cmbxAssign
+            // txtTaskName
             // 
-            this.cmbxAssign.DataSource = this.employeeBindingSource;
-            this.cmbxAssign.DisplayMember = "fName";
-            this.cmbxAssign.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbxAssign.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cmbxAssign.FormattingEnabled = true;
-            this.cmbxAssign.Location = new System.Drawing.Point(17, 412);
-            this.cmbxAssign.Name = "cmbxAssign";
-            this.cmbxAssign.Size = new System.Drawing.Size(316, 33);
-            this.cmbxAssign.TabIndex = 3;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(17, 195);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(703, 182);
-            this.textBox2.TabIndex = 1;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(17, 65);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(542, 30);
-            this.textBox1.TabIndex = 0;
+            this.txtTaskName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTaskName.Location = new System.Drawing.Point(19, 81);
+            this.txtTaskName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtTaskName.Name = "txtTaskName";
+            this.txtTaskName.Size = new System.Drawing.Size(609, 35);
+            this.txtTaskName.TabIndex = 0;
             // 
             // employeeTableAdapter
             // 
@@ -226,10 +237,11 @@
             // 
             // frmAddTask
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(763, 556);
+            this.ClientSize = new System.Drawing.Size(858, 695);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmAddTask";
             this.Text = "Add Task";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -244,13 +256,13 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtTaskDesc;
+        private System.Windows.Forms.TextBox txtTaskName;
         private System.Windows.Forms.ComboBox cmbxAssign;
         private dbtkDataSet dbtkDataSet;
         private System.Windows.Forms.BindingSource employeeBindingSource;
         private dbtkDataSetTableAdapters.EmployeeTableAdapter employeeTableAdapter;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStartDate;
         private System.Windows.Forms.ComboBox cmbxProj;
         private System.Windows.Forms.Label lbtskTitle;
         private System.Windows.Forms.Label ltskDesc;
--- EmployeeTracker/Form1.cs
+++ EmployeeTracker/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace EmployeeTracker
 {
     public partial class frmAddTask : Form
     {
+        OleDbConnection connection = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\jsantiago3\Downloads\dbtk.accdb");
         public frmAddTask()
         {
             InitializeComponent();
@@ -19,9 +21,84 @@ namespace EmployeeTracker
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'dbtkDataSet.Employee' table. You can move, or remove it, as needed.
-            this.employeeTableAdapter.Fill(this.dbtkDataSet.Employee);
-
+            PopulateComboBox();
         }
+
+        private void PopulateComboBox()
+        {
+            try
+            {
+                connection.Open();
+                string query = "SELECT EmployeeID, fName FROM Employee";
+                OleDbCommand command = new OleDbCommand(query, connection);
+                OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                cmbxAssign.DataSource = dataTable;
+                cmbxAssign.DisplayMember = "fName"; 
+                cmbxAssign.ValueMember = "EmployeeID";
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void cmbxAssign_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataRowView selectedRow = cmbxAssign.SelectedItem as DataRowView;
+            if (selectedRow != null)
+            {
+                string selectedItem = selectedRow["fname"].ToString();
+            }
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string taskName = txtTaskName.Text;
+            string taskDesc = txtTaskDesc.Text;
+            DateTime taskStartDate = dateTimePickerStartDate.Value;
+
+            if (cmbxAssign.SelectedItem != null)
+            {
+                DataRowView selectedRow = cmbxAssign.SelectedItem as DataRowView;
+                if (selectedRow != null)
+                {
+                    
+                    int employeeID = Convert.ToInt32(selectedRow["EmployeeID"]);
+
+                    InsertTask(taskName, taskDesc, taskStartDate, employeeID);
+                }
+            }
+        }
+
+        private void InsertTask(string taskName, string taskDesc, DateTime taskStartDate, int employeeID)
+        {
+            try
+            {
+                connection.Open();
+
+                OleDbCommand cmd = connection.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+
+                cmd.CommandText = "INSERT INTO Task(taskName, taskDesc, taskStartDate, EmployeeID) VALUES(@taskName, @taskDesc, @taskStartDate, @EmployeeID)";
+
+                cmd.Parameters.AddWithValue("@taskName", taskName);
+                cmd.Parameters.AddWithValue("@taskDesc", taskDesc);
+                cmd.Parameters.AddWithValue("@taskStartDate", taskStartDate);
+                cmd.Parameters.AddWithValue("@EmployeeID", employeeID);
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Task inserted successfully!");
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
     }
 }
